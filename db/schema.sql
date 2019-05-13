4884e5c7103230abc32b0b056ee9f6880f30d31a
Jay Neupane <jayesh.neupane@gmail.com>
2019-05-13 17:58:59 -0500

    commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
--- Create the database burgers_db.
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
--- Create the table burgers.
-CREATE TABLE burgers
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+CREATE TABLE burgers (
+	id INT (10) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR (100) NOT NULL,
+    devoured BOOLEAN,
+    date TIMESTAMP,
     PRIMARY KEY (id)
 );
-
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,16 @@
-USE burgers_db;
--- Insert new rows of data.
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("hamburger", false), ("cheeseburger", false), ("tofu burger", false);
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Beef Double Cheese Burger",
+0
+),
+(
+"Patty Melt Burger",
+0
+),
+(
+"Mushroom & Swiss Double Cheese Burger",
+0
+);
